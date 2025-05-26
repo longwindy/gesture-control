59a05e3ed3170f135782769f217171759640cb5e
longwindy <longwindy1@gmail.com>
2025-05-26 16:01:04 +0800

    05-26-16-00

--- keyboardConfig.py
+++ keyboardConfig.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from button import Button
-from config import button_interval
+from config import button_interval, button_size
 
 keys = [
     ["~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
@@ -11,12 +11,12 @@ keys = [
 ]
 
 custom_keys = {
-    "Tab" : {"key" : "Tab", "size": [140, 85]},
-    "Caps" : {"key" : "Caps", "size": [165, 85]},
-    "Shift" : {"key" : "Shift", "size": [190, 85]},
+    "Tab" : {"key" : "Tab", "size": [np.int32(button_size[0]*1.5), button_size[1]]},
+    "Caps" : {"key" : "Caps", "size": [np.int32(button_size[0]*1.75), button_size[1]]},
+    "Shift" : {"key" : "Shift", "size": [np.int32(button_size[0]*2.25), button_size[1]]},
 }
 
-def init_keyboard(start_x, start_y, buttonList, size=[85, 85], keys = keys, custom_keys = custom_keys):
+def init_keyboard(start_x, start_y, buttonList, size=button_size, keys = keys, custom_keys = custom_keys):
     current_x, current_y = start_x, start_y
     for i in range(len(keys)):
         current_y = start_y + i * (size[1] + button_interval)
